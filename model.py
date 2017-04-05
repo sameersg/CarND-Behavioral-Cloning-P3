963a1a10c671cac23f18f11324c5b4c32f80461c
Sameer <sameer@goswami.de>
2017-04-05 11:39:10 +0200

    still working

--- model.py
+++ model.py
@@ -2,9 +2,15 @@ import csv
 import cv2
 import numpy as np
 
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+
 lines = []
 
-with open('data_org/driving_log.csv') as csvfile:
+with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -16,23 +22,37 @@ for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
     #print(filename)
-    current_path = 'data_org/IMG/' + filename
+    current_path = 'data/IMG/' + filename
     #print(current_path)
-    image = cv2.imraead(current_path)
+    image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augumented_images, augumented_measurements = [], []
+for image, measuerement in zip(images, measurements):
+    augumented_images.append(image)
+    augumented_measurements.append(measuerement)
+    augumented_images.append(cv2.flip(image,1))
+    augumented_measurements.append(measuerement*-1.0)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+
+X_train = np.array(augumented_images)
+y_train = np.array(augumented_measurements)
+
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
+
+
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
 
