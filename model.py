ab18c8283086d0be2697164bc4ba6c609abdf1bf
Sameer <sameer@goswami.de>
2017-04-05 23:37:55 +0200

    Nvidia

--- model.py
+++ model.py
@@ -30,6 +30,7 @@ for line in lines:
     measurements.append(measurement)
 
 augumented_images, augumented_measurements = [], []
+
 for image, measuerement in zip(images, measurements):
     augumented_images.append(image)
     augumented_measurements.append(measuerement)
@@ -44,13 +45,15 @@ y_train = np.array(augumented_measurements)
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64,3,3, activation="relu"))
+model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 
